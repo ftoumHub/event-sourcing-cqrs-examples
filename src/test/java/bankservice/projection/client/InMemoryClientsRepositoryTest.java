04d4a61f10575f76752ce40958fc859d75231b5d
fedoFtoum <fedoftoum@linux.home>
2019-11-30 22:02:11 +0100

    updates

--- src/test/java/bankservice/projection/client/InMemoryClientsRepositoryTest.java
+++ src/test/java/bankservice/projection/client/InMemoryClientsRepositoryTest.java
@@ -4,6 +4,7 @@ import bankservice.domain.model.client.Email;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.UUID;
 
 import static org.hamcrest.CoreMatchers.equalTo;
@@ -13,30 +14,27 @@ public class InMemoryClientsRepositoryTest {
 
     public ClientRepository clientRepository = new InMemoryClientsRepository();
 
+    // Une liste d'ids de client à utiliser dans les tests
+    public List<UUID> clientIds = List.of(UUID.randomUUID(), UUID.randomUUID());
+
     @Test
     void getClientByUuid() {
-        UUID clientId = UUID.randomUUID();
-        String name = "Georges";
         Email email = new Email("georges.ginon@maif.fr");
 
-        clientRepository.save(new ClientProjection(clientId, name, email,1));
+        clientRepository.save(new ClientProjection(clientIds.get(0), "Georges", email,1));
 
-        assertThat(clientRepository.getClient(clientId).get().getName(), equalTo("Georges"));
+        assertThat(clientRepository.getClient(clientIds.get(0)).get().getName(), equalTo("Georges"));
     }
 
     @Test
     void getClientByEmail() {
-        UUID clientId1 = UUID.randomUUID();
-        String name1 = "Georges";
         String emailValue = "georges.ginon@maif.fr";
         Email email1 = new Email(emailValue);
 
-        UUID clientId2 = UUID.randomUUID();
-        String name2 = "John";
         Email email2 = new Email("john.doe@gmail.com");
 
-        clientRepository.save(new ClientProjection(clientId1, name1, email1,1));
-        clientRepository.save(new ClientProjection(clientId2, name2, email2,1));
+        clientRepository.save(new ClientProjection(clientIds.get(0), "Georges", email1,1));
+        clientRepository.save(new ClientProjection(clientIds.get(1), "John", email2,1));
 
         assertThat("L'email du client doit être identifiable",
                 clientRepository.getClientWithEmail(emailValue).get().getEmail().getValue(),
